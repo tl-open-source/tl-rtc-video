91e4000c6315e41cf5d1ef8e15650a8eea72babe
tsm <tsm000000>
2021-08-08 17:40:35 +0800

    1

--- serversocket.js
+++ serversocket.js
@@ -43,18 +43,5 @@ conf.ws.io = io;
 socket.excute(tables,conf);
 console.log("socket init...")
 
-app.all("im.tsmblogs.cn",function(req,res,next){
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin","*");
-  //允许的header类型
-  res.header("Access-Control-Allow-Headers","content-type");
-  //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-  if (req.method.toLowerCase() == 'options')
-    res.send(200);  //让options尝试请求快速结束
-  else
-    next();
-});
-
 
 console.log("socket listen on ",conf.ws.ssl_port," successful");
